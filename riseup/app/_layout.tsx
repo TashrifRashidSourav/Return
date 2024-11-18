0bd6db60162bac97110318bddc499ffbc975cc79
Md Tashrid Rashid Sourav <tashrifrashid.sourav@gmail.com>
2024-11-18 20:25:23 +0600

    testing_success

--- riseup/app/_layout.tsx
+++ riseup/app/_layout.tsx
@@ -1,12 +1,43 @@
-// app/_layout.tsx
-import React from 'react';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
 
-export default function Layout() {
+
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <Stack initialRouteName="authentication/login">
-      <Stack.Screen name="authentication/login" options={{ headerShown: false }} />
-      <Stack.Screen name="authentication/registration" options={{ headerShown: false }} />
-    </Stack>
+    
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+        <Stack.Screen name="(auth)" options={{ headerShown:false }} />
+        <Stack.Screen name="(auth)/login" options={{ headerShown:false }} />
+        <Stack.Screen name="./authentication/registration" options={{ headerShown:false }} />
+        <Stack.Screen name="index" options={{ headerShown:false }} />
+        
+        
+      </Stack>
+    
   );
 }
