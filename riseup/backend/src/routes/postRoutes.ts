bf568ba4b3db95994f2dce21a5b7db7a14005c2d
Md Tashrid Rashid Sourav <tashrifrashid.sourav@gmail.com>
2025-01-05 13:48:25 +0600

    post_update

--- riseup/backend/src/routes/postRoutes.ts
+++ riseup/backend/src/routes/postRoutes.ts
@@ -174,6 +174,7 @@ router.delete('/delete/:id', authenticate, async (req: Request, res: Response) =
     if (!post) return res.status(404).json({ message: 'Post not found or unauthorized' });
 
     res.status(200).json({ message: 'Post deleted successfully' });
+    
   } catch (error) {
     console.error('Error deleting post:', error);
     res.status(500).json({ message: 'Failed to delete post' });
